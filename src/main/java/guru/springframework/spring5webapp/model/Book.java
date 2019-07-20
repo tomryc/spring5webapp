ba7af7379de02ec4fe5d84d96550c82ded47cddb
Tomasz Rycerz <tomryc@gmail.com>
2019-07-20 13:38:36 +0100

    adding JPA example

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,15 +1,24 @@
 package guru.springframework.spring5webapp.model;
 
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
